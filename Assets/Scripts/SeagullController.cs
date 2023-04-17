bc374f6799f3ff06fe873ebf4d8f9b5462b1c7b8
Owen H <epik955@gmail.com>
2023-04-16 21:58:13 -0400

    Improve seagull AI

--- Assets/Scripts/SeagullController.cs
+++ Assets/Scripts/SeagullController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SeagullController : MonoBehaviour
 {
-    public Transform home;
+    public List<Transform> path;
     public float searchRadius = 5;
     public float idleSpeed = 1;
     public float attackSpeed = 1;
@@ -14,8 +15,11 @@ public class SeagullController : MonoBehaviour
     public Sprite attacking;
 
 
+    private List<Transform> reversePath;
     private PolygonCollider2D detector;
     private SeagullTalons talons;
+    private Sprite sprite;
+    private Transform home;
     public Vector3 curTarget;
 
     public enum State
@@ -26,13 +30,17 @@ public class SeagullController : MonoBehaviour
         returning
     }
     public State state;
+    private int pathIndex = 0;
 
 
     void Start()
     {
         state = State.searching;
+        home = path[0];
+        reversePath = Enumerable.Reverse(path).ToList();
         detector = GetComponent<PolygonCollider2D>();
         talons = GetComponentInChildren<SeagullTalons>();
+        sprite = Seagull.GetComponent<SpriteRenderer>().sprite;
     }
 
     void Update()
@@ -98,35 +106,50 @@ public class SeagullController : MonoBehaviour
     {
         Seagull.GetComponent<SpriteRenderer>().sprite = attacking;
         MoveToPoint(curTarget, attackSpeed);
+        if (Vector3.Distance(transform.position, curTarget) < 0.1f)
+        {
+            state = State.carrying;
+            pathIndex = 0;
+        }
     }
 
     // Flies to the left. If fish escapes or the level beginning is reached, set state to return
     void CarryingAction()
     {
-        Seagull.GetComponent<SpriteRenderer>().sprite = attacking;
-        MoveToPoint(curTarget, attackSpeed);
-        if (Vector3.Distance(transform.position, curTarget) < 0.1f)
+        FollowPath(path, attackSpeed);
+        if (Vector3.Distance(transform.position, path.Last().position) < 0.1f)
         {
             state = State.returning;
+            pathIndex = 0;
         }
     }
 
     // Disables talons, flies back to home. On reaching home, set state to searching and re-enable talons
     void ReturningAction()
     {
-        MoveToPoint(home.position, returnSpeed);
-        talons.Disengage();
         Seagull.GetComponent<SpriteRenderer>().sprite = flying;
+        FollowPath(reversePath, returnSpeed);
+        talons.Disengage();
         if (Vector3.Distance(transform.position, home.position) < 0.1f)
         {
             state = State.searching;
             talons.Engage();
+            pathIndex = 0;
         }
     }
 
     void MoveToPoint(Vector3 target, float speed)
     {
         transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    }
+
+    void FollowPath(List<Transform> path, float speed)
+    {
+        MoveToPoint(path[pathIndex].position, speed);
+        if (Vector3.Distance(transform.position, path[pathIndex].position) < 0.1f)
+        {
+            pathIndex++;
+        }
     }
 
     public Vector3 AroundHome()
