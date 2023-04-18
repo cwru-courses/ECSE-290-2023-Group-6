edcba38e9d1d94e781ff7a55fc8654f8ded0f4d0
Elwin He <50185565+elwinhe@users.noreply.github.com>
2023-04-18 00:28:08 -0400

    Add Urchin spike mechanic

--- Assets/Scripts/UrchinSpike.cs
+++ Assets/Scripts/UrchinSpike.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class UrchinSpike : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PointEffector2D grip;
+    private float gripForce;
+    private bool isGripping = false;
+    private UrchinSpike urchinSpike;
+
+    IEnumerator WaitToDisengage(float waitTime)
     {
-        
+        yield return new WaitForSeconds(waitTime);
+        Disengage();
+        isGripping = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        grip = GetComponent<PointEffector2D>();
+        gripForce = grip.forceMagnitude;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && !isGripping)
+        {
+            Engage();
+            isGripping = true;
+        }
+    }
+
+    public void Engage()
+    {
+        grip.forceMagnitude = gripForce;
+        StartCoroutine(WaitToDisengage(5f));
+    }
+
+    public void Disengage()
+    {
+        grip.forceMagnitude = 0;
     }
 }
