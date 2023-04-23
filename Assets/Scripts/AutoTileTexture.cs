a6114364b305e627cc4e7b5dc61140525aab03f8
Owen H <epik955@gmail.com>
2023-04-23 15:38:17 -0400

    Fix material issues on all finished levels

--- Assets/Scripts/AutoTileTexture.cs
+++ Assets/Scripts/AutoTileTexture.cs
@@ -4,23 +4,15 @@ using UnityEngine;
 
 public class AutoTileTexture : MonoBehaviour
 {
+    public bool isEnabled = false;
     public float scale = 1;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnDrawGizmos()
     {
-        Vector2 scaleVec = new Vector2(this.gameObject.transform.lossyScale.x / scale, this.gameObject.transform.lossyScale.y / scale);
-        this.gameObject.GetComponent<Renderer>().material.SetTextureScale("_MainTex", scaleVec);
+        if (isEnabled)
+        {
+            Vector2 scaleVec = new Vector2(this.gameObject.transform.lossyScale.x / scale, this.gameObject.transform.lossyScale.y / scale);
+            this.gameObject.GetComponent<Renderer>().material.SetTextureScale("_MainTex", scaleVec);
+        }
     } 
 }
